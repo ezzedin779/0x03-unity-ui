039a44edc0b0ce16cde2ff3950ca752fb24bba6f
Abderrahmen Babchia <2150@holbertonschool.com>
2021-10-13 00:14:43 +0200

    done

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+	public Material trap;
+	public Material goal;
+	public Toggle colorblind;
+
     // Start is called before the first frame update
     public void PlayMaze()
 	{
+		if (colorblind.isOn == true)
+		{
+			trap.color = new Color32(255, 112, 0, 1);
+			goal.color = Color.blue;
+		}
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 	// quit the game ?
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,25 +11,56 @@ public class PlayerController : MonoBehaviour
 	public float speed = 500f;
 	private int score = 0;
 	public int health = 5;
+	public Text scoreText;
+	public Text healthText;
+	public Text winloseText;
+	public Image winloseBG;
 
+	// Transition
+	IEnumerator LoadScene(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		health = 5;
+		score = 0;
+		SceneManager.LoadScene(0, LoadSceneMode.Single);
+	}
+
+	//Health text ?
+	void SetHealthText()
+	{
+		healthText.text = "Health: " + health.ToString();
+	}
+
+	// the score text
+	void SetScoreText()
+	{
+		scoreText.text = "Score: " + score.ToString();
+	}
 	// The pickup
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Pickup")
 		{
 			this.score += 1;
-			Debug.Log("Score: " + score);
+			SetScoreText();
+			//Debug.Log("Score: " + score);
 			other.GetComponent<Renderer>().enabled = false;
 			Destroy(other);
 		}
 		if (other.tag == "Trap")
 		{
 			this.health -= 1;
-			Debug.Log("Health: " + health);
+			SetHealthText();
+			//Debug.Log("Health: " + health);
 		}
 		if (other.tag == "Goal")
 		{
-			Debug.Log("You win!");
+			//Debug.Log("You win!");
+			winloseText.color = Color.black;
+			winloseText.text = "You win!";
+			winloseBG.color = Color.green;
+			winloseBG.gameObject.SetActive(true);
+			StartCoroutine(LoadScene(3));
 		}
 	}
     // Update is called once per frame
@@ -50,17 +82,27 @@ public class PlayerController : MonoBehaviour
 		{
 			rb.AddForce(0, 0,  -speed * Time.deltaTime);
 		}
-    }
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+	}
 
+	void Start()
+	{
+		winloseBG.gameObject.SetActive(false);
+	}
 	// Reload the game when dead
 	void Update()
 	{
 		if (health == 0)
 		{
-			Debug.Log("Game Over!");
-			health = 5;
-			score = 0;
-			SceneManager.LoadScene(0, LoadSceneMode.Single);
+			//Debug.Log("Game Over!");
+			winloseText.text = "Game Over!";
+			winloseText.color = Color.white;
+			winloseBG.color = Color.red;
+			winloseBG.gameObject.SetActive(true);
+			StartCoroutine(LoadScene(3));
 		}
 	}
 }
